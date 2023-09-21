d221ffe6c2062af79dc92a9493a30c6f0f6f0df1
Artem Mazin <minima1s@yandex.ru>
2023-09-21 23:00:08 +0300

    feat: сделать активные ссылки в хэдере

--- components/header/navigation/Navigation.jsx
+++ components/header/navigation/Navigation.jsx
@@ -1,46 +1,29 @@
+'use client';
+
 import React from 'react';
-import { row, list, item } from './navigation.module.css';
+import { row, list, item, active } from './navigation.module.css';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import navLinks from '@/utils/constants';
 
 const Navigation = () => {
+  const pathname = usePathname();
+
   return (
     <nav className={row}>
       <ul className={`${list} container`}>
-        <li>
-          <Link
-            href='/'
-            className={item}>
-            Главная
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/goods'
-            className={item}>
-            Товары
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/delivery'
-            className={item}>
-            Доставка и оплата
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/catalog'
-            className={item}>
-            Каталог
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/contacts'
-            className={item}>
-            Контакты
-          </Link>
-        </li>
+        {navLinks.map((link) => {
+          const isActive = pathname === link.href;
+          return (
+            <li key={link.name}>
+              <Link
+                href={link.href}
+                className={isActive ? `${item} ${active}` : `${item}`}>
+                {link.name}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+const navLinks = [
+  { name: 'Главная', href: '/' },
+  { name: 'Товары', href: '/goods' },
+  { name: 'Доставка и оплата', href: '/delivery' },
+  { name: 'Каталог', href: '/catalog' },
+  { name: 'Контакты', href: '/contacts' },
+];
+
+export default navLinks;
