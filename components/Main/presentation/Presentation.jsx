e9727e1328acb1e62c26bb8672c2b51c5e5ac43b
ArtemMazin <minima1s@yandex.ru>
2023-09-27 16:31:19 +0300

    style: доработать стили карточек

--- components/Main/presentation/Presentation.jsx
+++ components/Main/presentation/Presentation.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
-import { presentation, title, text, images, item } from './presentation.module.css';
+import {
+  presentation,
+  title,
+  text,
+  images,
+  item,
+  image,
+  materialNameContainer,
+  materialName,
+} from './presentation.module.css';
 import { presentationImage } from '@/utils/constants';
 import Image from 'next/image';
 
 const Presentation = () => {
   return (
-    <section className={presentation}>
-      <h2 className={`container ${title}`}>Metal-polymer self-lubricating bearings</h2>
-      <p className={`container ${text}`}>
-        Two-layer metal-polymer self-lubricating bearing consists of metal backing with PTFE compounded on the surface.
-        It is lighter and has better mechanical and load performance. The thicker PTFE layer can improve the
-        installation flexibility and can also match wider designed tolerance range. In addition, the wear resistance and
-        noise absorbing feature is improved while more variable torque range could be fitted.
-      </p>
+    <section className={`${presentation} container`}>
+      <h2 className={`container ${title}`}>НАША ПРОДУКЦИЯ</h2>
+      <p className={`container ${text}`}>HMG/ВТУЛКИ/ШАЙБЫ/ЛЕНТА</p>
       <ul className={images}>
         {presentationImage.map(({ name, img }) => {
           return (
             <li
               key={name}
-              className={item}>
+              className={item}
+              as={'image'}>
               <Image
                 src={img}
                 fill
                 sizes='(max-width: 1269px) 50vw, 22.5vw'
                 alt={name}
+                className={image}
+                priority={true}
               />
+              <div className={materialNameContainer}>
+                <h2 className={materialName}>{name}</h2>
+              </div>
             </li>
           );
         })}
