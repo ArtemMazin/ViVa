81fa26bd776284b4a5317fb72e2e6c165ee264f5
Artem Mazin <minima1s@yandex.ru>
2023-12-23 22:41:24 +0300

    feat: подключить я.метрику

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,22 +2,28 @@ import Header from '@/components/Header/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Footer from '@/components/Footer/Footer';
+import { Suspense } from 'react';
+import { Metrika } from '@/components/metrika';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Подшипники HMG',
   description: 'Подшипники скольжения HMG',
-  keywords: 'подшипники скольжения, втулки скольжения, самосмазывающие подшипники',
+  keywords:
+    'подшипники скольжения, втулки скольжения, самосмазывающие подшипники',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='ru'>
+    <html lang="ru">
       <body className={inter.className}>
         <Header />
         {children}
         <Footer />
+        <Suspense>
+          <Metrika />
+        </Suspense>
       </body>
     </html>
   );
